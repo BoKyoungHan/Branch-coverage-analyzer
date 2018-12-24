527c491ffddd64794762b4c0c07e7f9bffe7f3ac
BoKyoungHan <21500777@handong.edu>
2018-12-24 18:43:19 +0900

    read csv files automatically

--- drawGraph/iteration_graph.py
+++ drawGraph/iteration_graph.py
@@ -3,18 +3,23 @@ import numpy as np
 import csv
 import array
 import sys
+import os
 
 fig = plt.figure()
 fig.suptitle('iterations and covered branch')
-method = ['cfg', 'random', 'random_input', 'uniform_random','dfs_17', 'dfs_16', 'dfs_15', 'dfs_14']
+#method = ['cfg', 'random', 'random_input', 'uniform_random','dfs_17', 'dfs_16', 'dfs_15', 'dfs_14']
+
 
 try:
 	directory = sys.argv[1] #directory
 except:
-	print 'Usage: python iteration_graph.py <FILE_DIRECTORY>'
+	print('Usage: python iteration_graph.py <FILE_DIRECTORY>')
 	quit()
 
+file_list = os.listdir(directory)
+file_list.sort()
 
+print(file_list)
 
 
 #Get data
@@ -23,23 +28,29 @@ data = {}
 
 db = []
 
-for j in range(0, len(method)):
+for j in range(0, len(file_list)):
 
 	sum_for_coverage = {}
 	ave_for_coverage = {}
 	db = []
-	print(method[j])
+	print(file_list[j])
+	num_of_cov_file = 0
 	#TODO count coverages? files and do not use magic number
-	for i in range(0, 3):
+	sub_flie_list = os.listdir(directory + '/' + file_list[j])
+	for k in sub_flie_list:
+		if k[0:9] == 'coverages':
+			num_of_cov_file += 1 
+	print('cov_file' + str(num_of_cov_file))
+	for i in range(0, num_of_cov_file):
 		#Do initialize!!!
 		iteration = 0
 		data = {}
-		with open(directory + '/' + method[j] + '/coverages' + str(i) + '.csv', 'r') as raw:
+		with open(directory + '/' + file_list[j] + '/coverages' + str(i) + '.csv', 'r') as raw:
 			cooked = csv.reader(raw) #cooked: chunk of every record
 			for record in cooked: #record: ['coverage', 'time']
 				data[iteration] = record #data: {iteration, ['coverage', 'time']}
 	
-				if sum_for_coverage.has_key(iteration):
+				if iteration in sum_for_coverage:
 					sum_for_coverage[iteration] += int(record[0])
 				else:
 					sum_for_coverage[iteration] = int(record[0])
@@ -51,19 +62,19 @@ for j in range(0, len(method)):
 			
 
 	for k in range(0, iteration):
-		ave_for_coverage[k] = sum_for_coverage[k]/10.00
+		ave_for_coverage[k] = sum_for_coverage[k]/num_of_cov_file
 
 	x = [l for l in range(iteration)]
-	print 'x' + str(len(x))
+	print('x' + str(len(x)))
 	y = []
 	for l in range(0, iteration):
 		y.append(ave_for_coverage[l])
-	print 'y' + str(len(y))
+	print('y' + str(len(y)))
 
 
-	plt.plot(x, y, label = method[j])
+	plt.plot(x, y, label = file_list[j])
 
-	print 'strategy:' + method[j] + ', max covered branch: ' + str(ave_for_coverage[iteration-1])
+	print('strategy:' + file_list[j] + ', max covered branch: ' + str(ave_for_coverage[iteration-1]))
 
 plt.xlabel('iterations')
 plt.ylabel('coveraged branch')
